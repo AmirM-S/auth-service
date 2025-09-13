8aa5e6463bee0eee0bae86303f1fc4906a6b30f2
MasonS <amsadeghi79@gmail.com>
2025-09-13 22:04:56 +0330

    update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Request } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('profile')
+  async getProfile(@Request() req) {
+    return this.usersService.findOne(req.user.id)
+  }
+
+  @Patch('profile')
+  async updateProfile(@Request() req, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(req.user.id, updateUserDto);
+  }
+
+  @Get('permissions')
+  async getPermissions(@Request() req) {
+    const permissions = await this.usersService.getUserPermissions(req.user.id);
+    return { permissions };
+  }
 }
