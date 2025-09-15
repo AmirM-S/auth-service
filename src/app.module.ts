5d9907febbf7c06124b26ab78462a0b12f041679
MasonS <amsadeghi79@gmail.com>
2025-09-15 20:10:33 +0330

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
 import { AuthModule } from './auth/auth.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     UsersModule,
     AuthModule,
+    MailModule
   ],
   controllers: [AppController],
   providers: [AppService],
