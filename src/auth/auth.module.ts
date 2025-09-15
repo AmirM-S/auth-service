696357bc8d6046633096a07bb68845e72cac4646
MasonS <amsadeghi79@gmail.com>
2025-09-15 21:28:10 +0330

    update auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from '../users/users.module';
+import { MailModule } from '../mail/mail.module';
+import { RefreshToken } from './entities/refresh-token.entity';
+import { AuthEvent } from './entities/auth-event.entity';
+import { LoginAttempt } from './entities/login-attempt.entity';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
+import { TokenService } from './services/token.service';
+import { SecurityService } from './services/security.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([RefreshToken, AuthEvent, LoginAttempt]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRATION', '15m') },
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    MailModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy, LocalStrategy, TokenService, SecurityService],
+  exports: [AuthService, TokenService],
 })
 export class AuthModule {}
