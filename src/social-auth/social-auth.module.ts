b4c21c1158788c2f750fc520e16a940d9f1ba09b
MasonS <amsadeghi79@gmail.com>
2025-09-16 21:38:48 +0330

    feat(social-auth): add GoogleStrategy and configure module imports

--- src/social-auth/social-auth.module.ts
+++ src/social-auth/social-auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { SocialAuthService } from './social-auth.service';
 import { SocialAuthController } from './social-auth.controller';
+import { SocialAccount } from './entities/social-account.entity';
+import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
+import { GoogleStrategy } from './strategies/google.strategy';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([SocialAccount]),
+    PassportModule,
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [SocialAuthController],
-  providers: [SocialAuthService],
+  providers: [SocialAuthService, GoogleStrategy],
+  exports: [SocialAuthService],
 })
 export class SocialAuthModule {}
