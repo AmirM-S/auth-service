db32202c27ff3704212d2fc03e53a72868f0735f
MasonS <amsadeghi79@gmail.com>
2025-09-16 22:31:04 +0330

    feat(admin-module): add TypeOrm integration with user, role, permission, and auth event entities

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
+import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
+import { User } from '../users/entities/user.entity';
+import { Role } from '../users/entities/role.entity';
+import { Permission } from '../users/entities/permission.entity';
+import { AuthEvent } from '../auth/entities/auth-event.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, Role, Permission, AuthEvent]),
+    UsersModule,
+    AuthModule,
+  ],
   controllers: [AdminController],
   providers: [AdminService],
 })
